ce0c61c974b815b60dc701d8690bf08e5d11ecb0
SamudraThio <68200757+SamudraThio@users.noreply.github.com>
2020-09-23 16:17:51 -0500

    Added ASP.NET Sports Store Tutorial

---
+++ Tutorials/SportsSln/SportsStore/Views/_ViewImports.cshtml.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SportsStore.Models.ViewModels;
+
+
+namespace SportsStore.Views
+{
+    public class _ViewImportsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
