457114b1eec49b30e55c8dc6b89e6b3c1a8283fc
SamudraThio <68200757+SamudraThio@users.noreply.github.com>
2020-09-29 11:49:13 -0500

    Making a collection with enumerators in class, following Elliot Robinson.

---
+++ Tutorials/Collections and Enumerators/Collections and Enumerators/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collections_and_Enumerators
+{
+    public class MyList<T> : IEnumerable<T>
+    {
+        // Contains Ts
+        T[] backingStore = new T[1];
+
+        // Track the number of Ts currently stored
+        public int Length { get; private set; } = 0;
+        public int Capacity => backingStore.Length;
+
+        // Grow the backing array when needed
+        private void GrowBackingArrayIfNeeded()
+        {
+            // Determine whether the array needs to be grown
+            if (Length == backingStore.Length)
+            {
+                // If so, make a new larger array
+                var tmp = new T[backingStore.Length * 2];
+
+                // Copy the values from the old array into the new array
+                for (int i = 0; i < backingStore.Length; i++)
+                {
+                    tmp[i] = backingStore[i];
+                }
+
+                // Replace the old array  
+                backingStore = tmp;
+            }
+        }
+
+        // Indexer 
+        public T this[int index]
+        {
+            // Get and set value at index
+            get => index < Length ? backingStore[index] 
+                                  : throw new IndexOutOfRangeException();
+            set
+            {
+                if (index <= Length)
+                {
+                    backingStore[index] = value;
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException();
+                }
+            }
+        }
+
+        public void AddAtIndex(int index, T value)
+        {
+            //Only if the index is within the bounds of the current list
+            if (index < Length)
+            {
+                // Move all elements in or after the index to the right
+                GrowBackingArrayIfNeeded();
+
+                for (int i = Length ; i > index ; i--)
+                {
+                    backingStore[i] = backingStore[i - 1];
+                }
+                // Put the new value in the array
+                backingStore[index] = value;
+                Length += 1;
+            }
+        }
+
+        // Remove value
+
+        // Add value to end
+        public void Add(T value)
+        {
+            GrowBackingArrayIfNeeded();
+            backingStore[Length++] = value;
+        }
+        
+        // Remove value at index
+        public void RemoveAtIndex(int index)
+        {
+            while (index + 1 < Length)
+            {
+                backingStore[index] = backingStore[index + 1];
+                index += 1;
+            }
+            Length -= 1;
+        }
+
+
+        public void Shrink()
+        {
+            var tmp = new T[Length];
+
+            Array.Copy(backingStore, tmp, Length);
+
+            backingStore = tmp;
+        }
+
+        // Enumerable
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                yield return backingStore[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new MyList<int>();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+
+            foreach(var v in list.Reverse())
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
---
+++ Tutorials/Collections and Enumerators/Collections and EnumeratorsTests/MyListTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Collections_and_Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections_and_Enumerators.Tests
+{
+    [TestClass()]
+    public class MyListTests
+    {
+        [TestMethod()]
+        public void AddAtIndexTest()
+        {
+            // Arrange
+            var list = new MyList<int>();
+
+            // Act
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.AddAtIndex(2, 7);
+
+            // Assert
+            Assert.AreEqual(1, list[0]);
+            Assert.AreEqual(2, list[1]);
+            Assert.AreEqual(7, list[2]);
+            Assert.AreEqual(3, list[3]);
+            Assert.AreEqual(4, list[4]);
+            Assert.AreEqual(5, list.Length);
+            Assert.AreEqual(8, list.Capacity);
+        }
+
+        [TestMethod()]
+        public void RemoveFromIndexTest()
+        {
+            // Arrange
+            var list = new MyList<int>();
+
+            // Act
+            list.Add(1);
+            list.Add(2);
+            list.Add(3); // To be removed
+            list.Add(4);
+            list.Add(5);
+            list.RemoveAtIndex(2);
+            list.Shrink();
+
+            // Assert
+            Assert.AreEqual(1, list[0]);
+            Assert.AreEqual(2, list[1]);
+            Assert.AreEqual(4, list[2]);
+            Assert.AreEqual(5, list[3]);
+            Assert.AreEqual(4, list.Length);
+            Assert.AreEqual(list.Length, list.Capacity);
+        }
+    }
+}
