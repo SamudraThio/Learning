a9f4e76a13bac1a23b8787b66f58d68e974b456a
SamudraThio <68200757+SamudraThio@users.noreply.github.com>
2020-09-30 13:17:56 -0500

    Added Inventory, LootTable and QuestList.

--- Tutorials/SuperAdventure/Engine/Location.cs
+++ Tutorials/SuperAdventure/Engine/Location.cs
@@ -17,6 +17,26 @@ namespace Engine
             ID = id;
             Name = name;
             Description = description;
-        }    
+        }
+        
+        public Item ItemRequiredToEnter { get; set; }
+        public Quest QuestAvailableHere { get; set; }
+        public Monster MonsterLivingHere { get; set; }
+        public Location LocationToNorth { get; set; }
+        public Location LocationToEast { get; set; }
+        public Location LocationToSouth { get; set; }
+        public Location LocationToWest { get; set; }
+
+        public Location(int id, string name, string description, Item itemRequiredToEnter = null
+                       ,Quest questAvailableHere = null, Monster monsterLivingHere = null)
+        {
+            ID = id;
+            Name = name;
+            Description = description;
+            ItemRequiredToEnter = itemRequiredToEnter;
+            QuestAvailableHere = questAvailableHere;
+            MonsterLivingHere = monsterLivingHere;
+
+        }
     }
 }
