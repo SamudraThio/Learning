1bc5680bdff5c34e7921d985911e8d26edc233ae
SamudraThio <68200757+SamudraThio@users.noreply.github.com>
2020-10-05 09:24:14 -0500

    Delegate added.

---
+++ Tutorials/DelegatesandEvents/Delegates and Events/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+
+namespace Delegates_and_Events
+{
+    class Program
+    {
+
+        static char Rot13(char c)
+        {
+            if (c >= 'a' && c <= 'z')
+            {
+                c -= 'a';
+                c += (char)13;
+                c %= (char)26;
+                c += 'a';
+            }
+
+            return c;
+        }
+
+        private static void TestRot13()
+        {
+            Console.WriteLine("hello, world");
+            Console.WriteLine("hello, world".Select(Rot13).ToArray());
+            Console.WriteLine("hello, world".Select(Rot13).ToArray()) ;
+        }
+
+        static void AddOneToEachValueInIntArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] += 1;
+            }
+        }
+
+        static void AddTenToEachValueInIntArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] += 10;
+            }
+        }
+
+        static void ModFiveToEachValueInIntArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] %= 5;
+            }
+        }
+
+        static void DoSomethingToEachValueInIntArray(int[] array, IntToIntMethod Something)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Something(array[i]);
+            }
+        }
+
+        private delegate int IntToIntMethod(int value);
+
+        static void Main(string[] args)
+        {
+            TestRot13();
+
+            var values = Enumerable.Range(1, 10).ToArray();
+
+            AddOneToEachValueInIntArray(values);
+            AddTenToEachValueInIntArray(values);
+            ModFiveToEachValueInIntArray(values);
+
+            foreach (var v in values)
+            {
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
